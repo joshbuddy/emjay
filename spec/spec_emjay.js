c33dd659f6a66c10b591b448aa2a8bcb5c338412
Joshua Hull <joshbuddy@gmail.com>
2010-01-17 04:51:44 -0500

    alternate syntax

--- spec/spec_emjay.js
+++ spec/spec_emjay.js
@@ -2,38 +2,38 @@ require('../lib/emjay');
 
 describe("Emjay", function() {
   it("should interpolate a simple value", function() {
-    var template = new Emjay("test`1`");
+    var template = new Emjay("test=`1`");
     assertEqual('test1', template.run());
   });
 
   it("should interpolate a more complex example", function() {
-    var template = new Emjay("test`'test' + 3`");
+    var template = new Emjay("test=`'test' + 3`");
     assertEqual('testtest3', template.run());
   });
   
-  it("should not interpolate if it starts with ``", function() {
-    var template = new Emjay("test`` 1+1`");
+  it("should not interpolate if it starts with `", function() {
+    var template = new Emjay("test`1+1`");
     assertEqual('test', template.run());
   });
   
-  it("should not interpolate if it starts with ``", function() {
-    var template = new Emjay("test`` 1+1`");
+  it("should not interpolate if it starts with `", function() {
+    var template = new Emjay("test`1+1`");
     assertEqual('test', template.run());
   });
   
   it("should preserve variables declared in it, but not on subsequent runs", function() {
-    var template = new Emjay("``try {val += 1} catch(e) {}; val = 1`test`val`\n``val += 1`");
-    assertEqual('test1\n', template.run());
-    assertEqual('test1\n', template.run());
+    var template = new Emjay("`try {val += 1} catch(e) {}; val = 1`test=`val``val += 1`");
+    assertEqual('test1', template.run());
+    assertEqual('test1', template.run());
   });
   
   it("should reference variables bound at eval-time", function() {
-    var template = new Emjay("test`val`");
+    var template = new Emjay("test=`val`");
     assertEqual('test1', template.run({val: 1}));
   });
-
+  
   it("should reference and call variables bound at eval-time", function() {
-    var template = new Emjay("test`val(1)`");
+    var template = new Emjay("test=`val(1)`");
     assertEqual('test1', template.run({val: function(e) {return e}}));
   });
 
