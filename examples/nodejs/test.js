baf5451862c00b1fe037e5d79203be0465fea748
Joshua Hull <joshbuddy@gmail.com>
2010-01-25 03:29:48 -0500

    removed globals

--- examples/nodejs/test.js
+++ examples/nodejs/test.js
@@ -9,17 +9,32 @@ HTMLHelper = {
       buf += ' ' + this.escapeHtml(key) + '="'+this.escapeHtml(attrs[key]) + '"'
     }
     if (func) {
-      __runtime.write((buf + '>').makeSafe());
+      this.write((buf + '>').makeSafe());
       func();
-      __runtime.write(('</'+name+'>').makeSafe());
+      this.write(('</'+name+'>').makeSafe());
     } else {
       return (buf + '>').makeSafe();
     }
   }
 }
 
-new Emjay.NodeJs([HTMLHelper, Emjay.NodeJs.Helpers]).load('./examples/nodejs/test.mjs').run({title: 'test title'}, function(rendered) {
-  sys.puts('--------------------------------- RENDERED');
-  sys.puts(rendered);
-  sys.puts('\n--------------------------------- RENDERED');
-});
+var sys = require("sys"),
+    http = require("http");
+http.createServer(function (request, response) {
+  var emjay = new Emjay.NodeJs([HTMLHelper, Emjay.NodeJs.Helpers]);
+  emjay.load('./examples/nodejs/test.mjs').run({title: 'test title'}, function(rendered) {
+    response.sendHeader(200, {"Content-Type": "text/plain"});
+    response.sendBody(rendered);
+    response.finish();
+  });
+}).listen(8000);
+sys.puts("Server running at http://127.0.0.1:8000/");
+
+//
+//
+//new Emjay.NodeJs([HTMLHelper, Emjay.NodeJs.Helpers]).load('./examples/nodejs/test.mjs').run({title: 'test title'}, function(rendered) {
+//  sys.puts('--------------------------------- RENDERED');
+//  sys.puts(rendered);
+//  sys.puts('\n--------------------------------- RENDERED');
+//});
+//
